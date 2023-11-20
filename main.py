7c03b640ebfb9dfcbdfdfd0a92eed107877ca5c8
MP4-Player <markbulgarov@yandex.ru>
2023-11-21 02:08:48 +0300

    должно отправиться

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ app = FastAPI()
 
 async def get_body(request: Request):
     return await request.body()
-#uvicorn main:app --reload
+###uvicorn main:app --reload
 @app.get("/{user_id}")
 def read_user(user_id: str):
     return {"user_id": user_id}
 @app.get("/")
-#def read_user(url: str ):
+###def read_user(url: str ):
 def read_user(url: str ,HTMLelement):
     filtered_titles = []
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     testing=str(HTMLelement)
     titles = soup.findAll(testing)
-    #titles = soup.findAll(testing)
-    #titles = soup.findAll('p')
+####    #titles = soup.findAll(testing)
+####    #titles = soup.findAll('p')
     for data in titles:
         filtered_titles.append(data.text)
     return {"result": preprocessing(filtered_titles)}
